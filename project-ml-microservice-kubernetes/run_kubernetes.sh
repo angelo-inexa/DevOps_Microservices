40105d1e02704a6f45c461d0e9c5067dfc1f63d5
Ubuntu <ubuntu@ip-172-31-42-59.ec2.internal>
2022-08-07 19:05:34 +0000

    first commit of my projet after refactoring config.yml

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=angelo20/microserviceproject:v1.0.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microserviceproject \
+        --image=angelo20/microserviceproject:v1.0.0 \
+        --port=80 --labels app=microserviceproject
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward microserviceproject 8000:80
